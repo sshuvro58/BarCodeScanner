614ce8310758531fd58a43cc2097f115286ce92b
sshuvro58 <sshuvro58@gmail.com>
2016-06-15 02:35:18 +0600

    first commit

---
+++ app/src/main/java/com/shuvro/barcodescanner/CaptureActivityAnyOrientation.java
@@ -0,0 +1,10 @@
+package com.shuvro.barcodescanner;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Created by root on 6/7/16.
+ */
+public class CaptureActivityAnyOrientation extends CaptureActivity {
+
+}
---
+++ app/src/main/java/com/shuvro/barcodescanner/MainActivity.java
@@ -0,0 +1,61 @@
+package com.shuvro.barcodescanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    String scanContent;
+    String scanFormat;
+    TextView textView;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator scanIntegrator = new IntentIntegrator(MainActivity.this);
+                scanIntegrator.setPrompt("Scan a Barcode");
+                scanIntegrator.setBeepEnabled(true);
+                scanIntegrator.setCaptureActivity(CaptureActivityAnyOrientation.class);
+                scanIntegrator.setOrientationLocked(true);
+                scanIntegrator.setBarcodeImageEnabled(true);
+                scanIntegrator.initiateScan();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanningResult != null) {
+            if (scanningResult.getContents() != null) {
+                scanContent = scanningResult.getContents().toString();
+                scanFormat = scanningResult.getFormatName().toString();
+            }
+
+            Toast.makeText(this,scanContent+"   type:"+scanFormat,Toast.LENGTH_SHORT).show();
+
+            textView.setText(scanContent+"    type:"+scanFormat);
+
+        }else{
+            Toast.makeText(this,"Nothing scanned",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
